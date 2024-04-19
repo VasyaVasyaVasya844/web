a9c4d55c3bd176f41552f002b705eb355dfe6615
VasyaVasyaVasya844 <dragon04-04@mail.ru>
2024-04-19 23:44:11 +0300

    first commit

--- 6-map.js
+++ 6-map.js
@@ -1,6 +1,7 @@
+// BEGIN
 const getChildren = (users) => {
-    return users.reduce((children, user) => children.concat(user.children), []);
-  };
-  
-  export default getChildren;
-  
+  return users.reduce((children, user) => children.concat(user.children), []);
+};
+
+export default getChildren;
+// END
